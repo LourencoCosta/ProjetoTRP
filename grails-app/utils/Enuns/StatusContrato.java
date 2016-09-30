e2fde0ba408256aa969fe74fd2e7d3e3790130b4
RLC <rafaelconteudossi@gmail.com>
2016-09-30 19:06:44 -0300

    #master - enuns de status - WIP

---
+++ grails-app/utils/Enuns/StatusContrato.java
@@ -0,0 +1,21 @@
+package Enuns;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rlc
+ */
+public enum StatusContrato {
+    
+    VIGENTE,
+    FINALIZADO,
+    CANCELADO;
+
+    StatusContrato() {
+    }
+}
